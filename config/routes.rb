2e53e44e574809ab619acf0c2b5e23b9c46e62ff
Yonatha Almeida <yonathalmeida@gmail.com>
2021-02-09 19:11:24 -0300

    Implementado o cálculo da receita bruta e quantidade de registros

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "pessoas#importar"
+
+  resources :pessoas do
+    collection do
+      post :processar
+    end
+  end
 end
