53100f34755ee643d1c38ddbd61134a2f9486e13
Yonatha Almeida <yonathalmeida@gmail.com>
2021-02-15 10:53:02 -0300

    Finalizado os testes no model Pessoa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210209140227) do
+ActiveRecord::Schema.define(version: 20210213142300) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pessoas", force: :cascade do |t|
     t.string  "lote"
--- test/models/pessoa_test.rb
+++ test/models/pessoa_test.rb
@@ -1,7 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class PessoaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Salvar pessoa preenchendo todos os atributos' do
+    pessoa = pessoas(:completo)
+    assert pessoa.valid?
+  end
+
+  test 'Salvar pessoa preenchendo não informando todos os atributos' do
+    pessoa = pessoas(:incompleto)
+    assert_not pessoa.valid?, pessoa.errors.full_messages.inspect
+  end
+
+  test 'Todos os atributos vazios' do
+    pessoa = pessoas(:vazia)
+    assert_not pessoa.valid?, pessoa.errors.full_messages.inspect
+  end
+
+  test 'O preço não pode ser negativo' do
+    pessoa = pessoas(:preco_invalido)
+    assert_not pessoa.preco >= 0
+  end
+
+  test 'A quantidade não pode ser negativa' do
+    pessoa = pessoas(:quantidade_invalida)
+    assert_not pessoa.quantidade >= 0
+  end
 end
