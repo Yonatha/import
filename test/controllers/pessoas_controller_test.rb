08f39844879abd0bb933c5e10608cb88b3824c8f
Yonatha Almeida <yonathalmeida@gmail.com>
2021-02-15 11:42:52 -0300

    Finalizado os testes no PessoaController

--- test/controllers/pessoas_controller_test.rb
+++ test/controllers/pessoas_controller_test.rb
@@ -6,5 +6,19 @@ class PessoasControllerTest < ActionController::TestCase
         assert_response :success
     end
 
+    test "Fazer upload de arquivo válido" do
+        patch = '/files/dados_validos.txt'
+        post :create, params: {
+          :arquivo => fixture_file_upload(patch, 'text/txt')
+        }
+        assert_select ".alert", "Dados importados com sucesso"
+    end
 
+    test "Fazer upload de extensão inválida" do
+        patch = '/files/dados_invalidos.pdf'
+        post :create, params: {
+          :arquivo => fixture_file_upload(patch, 'text/txt')
+        }
+        assert_select ".alert", "Extensão inválida"
+    end
 end
